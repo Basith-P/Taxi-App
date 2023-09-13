b794debe0813266f818f3d9d842870f984ce2730
Basith P <basithpofficial@gmail.com>
2023-09-13 19:55:29 +0530

    add firebase

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:taxi_app/app/app.dart';
 import 'package:taxi_app/bootstrap.dart';
 
-void main() {
-  bootstrap(() => const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await bootstrap(() => const App());
 }
