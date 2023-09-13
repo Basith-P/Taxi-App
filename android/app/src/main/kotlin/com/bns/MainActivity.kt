8160416d61d8a7d87696fce6001ff781e96d975f
Basith P <basithpofficial@gmail.com>
2023-09-13 19:42:04 +0530

    start project

---
+++ android/app/src/main/kotlin/com/bns/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bns.taxi_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:taxi_app/app/app.dart';
+import 'package:taxi_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
